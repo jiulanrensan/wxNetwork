c6cae028c8d98f6cae75f040534df40160a1dc1c
20190225001 <839492148@qq.com>
2022-05-01 23:13:39 +0800

    初始化git仓库；network按钮样式完成；拖拽交互完成

---
+++ wxNetwork/index.js
@@ -0,0 +1,53 @@
+// wxNetwork/index.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+		windowHeight: 603,
+		windowWidth: 375,
+		networkWidth: 65,
+		networkHeight: 22
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		setNetworkInfo () {
+			const that = this
+			const styleList = ['width', 'height', 'padding-left', 'padding-right', 'padding-top', 'padding-bottom']
+			this.createSelectorQuery().select('#wxNetwork').fields({
+				computedStyle: styleList
+			}, function (res) {
+				const styleMap = {}
+				styleList.forEach(el => {
+					styleMap[el] = Number(res[el].replace('px', ''))
+				})
+				that.setData({
+					networkWidth: styleMap['width'] + styleMap['padding-left'] + styleMap['padding-right'],
+					networkHeight: styleMap['height'] + styleMap['padding-top'] + styleMap['padding-bottom']
+				})
+			}).exec()
+		}
+	},
+	lifetimes: {
+		created () {
+			const { windowHeight, windowWidth } = wx.getSystemInfoSync()
+			this.setData({
+				windowHeight,
+				windowWidth
+			})
+		},
+		attached () {
+			this.setNetworkInfo()
+		}
+	}
+})
